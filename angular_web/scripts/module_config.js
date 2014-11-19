27a180b2b5d4728a8556ccf3584e0dfd23b30187
lhlPersonal <724800840@qq.com>
2014-11-19 12:10:03 +0800

    create webchat.html

--- angular_web/scripts/module_config.js
+++ angular_web/scripts/module_config.js
@@ -2,13 +2,10 @@
  * Created by bulusli on 2014/10/15.
  */
 'use strict';
-var app = angular.module('invlist_module', ['ngRoute','ngGrid']);
+var app = angular.module('module', ['ngRoute']);
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/add', {templateUrl: '/views/template/invlist/add.html'})
-        .when('/ilist/detail/:id', {templateUrl: '/views/template/invlist/devList.html'})
-        .otherwise({redirectTo: '/views/inventory.html'});
+    $routeProvider.otherwise({redirectTo: 'index.html'});
 
     $locationProvider.html5Mode(true);
 }])
